fd460ff32a378cd21b30d0e4d71c1cbcf8dd9dc5
vijayvidhyut <vijayvidhyut@gmail.com>
2024-08-26 14:06:47 +0530

    java added

---
+++ src/hellojava/hello.java
@@ -0,0 +1,15 @@
+package hellojava;
+
+public class hello {
+  public static void main(String[] args) {
+	System.out.println("hello java");
+	int i,j;
+	for(i=0;i<=5;i++) {
+		for(j=0;j<=i;j++) {
+			System.out.print("*");
+		}
+      System.out.println();
+		
+	}
+}
+}
